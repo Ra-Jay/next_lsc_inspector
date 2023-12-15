24b42edf4998e6af9f156a07865b8478e59e10ee
kerrlabajo <labajokerr5@gmail.com>
2023-12-15 14:12:44 +0800

    Remove production API URL

--- src/services/config.js
+++ src/services/config.js
@@ -4,9 +4,6 @@ let apiUrl = null
 
 if (isLocal) {
 	apiUrl = 'http://localhost:5000/api/v1'
-} else {
-	// Production
-	apiUrl = 'https://lsc-inspector-908ed02924c7.herokuapp.com/api/v1'
 }
 
 const config = {
